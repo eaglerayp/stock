783d1d1ca5066a7de5467d7bc47615b4d7f1fd3e
eaglerayp <eagle_rayp@hotmail.com>
2017-07-27 09:42:45 +0800

    update: parse excel done

--- datatodb/src/save/main.go
+++ datatodb/src/save/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 
+	"gopkg.in/mgo.v2/bson"
+
 	"github.com/tealeg/xlsx"
 )
 
@@ -23,7 +25,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+// TODO: connect mongodb
 	for _, file := range fileList {
 		// read excel from data folder
 		fmt.Println(file)
@@ -36,16 +38,45 @@ func main() {
 		fmt.Println(len(xlFile.Sheets))
 		for _, sheet := range xlFile.Sheets {
 			// rows end with data is null
-			for _, row := range sheet.Rows {
-				// row 5 cell 1 is stock name (just take number to create collection), and labels.
-				// get labels []string
-				n := len(cells)
-				for _, cell := range row.Cells {
+			// read row 5 to get stock name, and labels
+
+			labelRaw := sheet.Rows[4]
+			cols := len(labelRaw.Cells)
+			labels := make([]string, cols)
+			stockName := labelRaw.Cells[0].String()
+			// TODO: add mgo collection
+			fmt.Println("stock:", stockName)
+			for i := 1; i < cols; i++ {
+				labels[i] = labelRaw.Cells[i].String()
+			}
+			fmt.Println("labels:", labels)
+			rawsNum := len(sheet.Rows)
+			if rawsNum > 1365 {
+				rawsNum = 1365
+			}
+			for i := 5; i < rawsNum; i++ {
+				raw := sheet.Rows[i]
+				data := bson.M{}
+				cols = len(raw.Cells)
+				for j := 1; j < cols; j++ {
 					// from cell 2 ~  n
 					// try int, float, then string
-					text := cell.String()
-					fmt.Printf("%s\n", text)
+					key := labels[j]
+					ivalue, err := raw.Cells[j].Int()
+					if err == nil {
+						data[key] = ivalue
+						continue
+					}
+					fvalue, err := raw.Cells[j].Float()
+					if err == nil {
+						data[key] = fvalue
+						continue
+					}
+					svalue := raw.Cells[j].String()
+					data[key] = svalue
 				}
+				// TODO:bulk insert
+				fmt.Println(data)
 			}
 		}
 		// save to mongodb
