0f77db30f1743bb134c101b4df8f53b9fce4a98d
eaglerayp <eagle_rayp@hotmail.com>
2017-07-28 18:14:25 +0800

    update: add mgo

--- datatodb/src/save/main.go
+++ datatodb/src/save/main.go
@@ -5,10 +5,16 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
+	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
 	"github.com/tealeg/xlsx"
+)
+
+const (
+	db = "stock"
 )
 
 func main() {
@@ -25,17 +31,38 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-// TODO: connect mongodb
+	// TODO: connect mongodb
+
+	dialInfo := mgo.DialInfo{
+		Addrs:     []string{"127.0.0.1:20379"},
+		FailFast:  true,
+		Username:  "",
+		Password:  "",
+		PoolLimit: 1,
+	}
+	maxAttempts := 20
+	var session *mgo.Session
+	for attempts := 1; attempts <= maxAttempts; attempts++ {
+		session, err = mgo.DialWithInfo(&dialInfo)
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Duration(attempts) * time.Second)
+	}
+	if err != nil {
+		panic("[mongo] no reachable server")
+	}
+
 	for _, file := range fileList {
 		// read excel from data folder
-		fmt.Println(file)
+		fmt.Println("File:", file)
 
 		xlFile, err := xlsx.OpenFile(file)
 		if err != nil {
 			log.Println("open excel fail:", err)
 			panic(err)
 		}
-		fmt.Println(len(xlFile.Sheets))
+
 		for _, sheet := range xlFile.Sheets {
 			// rows end with data is null
 			// read row 5 to get stock name, and labels
@@ -44,12 +71,15 @@ func main() {
 			cols := len(labelRaw.Cells)
 			labels := make([]string, cols)
 			stockName := labelRaw.Cells[0].String()
+			col := session.DB(db).C(stockName)
+			bulk := col.Bulk()
+			bulk.Unordered()
 			// TODO: add mgo collection
 			fmt.Println("stock:", stockName)
 			for i := 1; i < cols; i++ {
 				labels[i] = labelRaw.Cells[i].String()
 			}
-			fmt.Println("labels:", labels)
+			// fmt.Println("labels:", labels)
 			rawsNum := len(sheet.Rows)
 			if rawsNum > 1365 {
 				rawsNum = 1365
@@ -76,7 +106,12 @@ func main() {
 					data[key] = svalue
 				}
 				// TODO:bulk insert
-				fmt.Println(data)
+				bulk.Insert(data)
+				// fmt.Println(data)
+			}
+			_, err = bulk.Run()
+			if err != nil {
+				log.Println("bulk insert error:", err)
 			}
 		}
 		// save to mongodb
